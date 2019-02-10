cec538a8f70cc6dfda53e9af9c7f9ca51d972fce
Juan Pablo Perez <Juan.Pablo.Perez@retailinmotion.com>
2019-02-10 07:16:21 +0000

    9. GraphQL Arguments, Variables, and Page Queries

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 
-export default () => (
+const PostTemplate = ({ data: post }) => (
   <Layout>
-    <h2>Programmatically created blog post!</h2>
+    <h1>{post.markdownRemark.frontmatter.title}</h1>
+    <div dangerouslySetInnerHTML={{ __html: post.markdownRemark.html }} />
   </Layout>
 )
+
+// This will be rendered and sent to the `data` parameter
+export const query = graphql`
+  query($slug: String) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
+
+export default PostTemplate
